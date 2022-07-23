83f578f01e93722e6562f6be2d2c9394ae4d59ad
FilipWirsen <Filip.wirsen99@gmail.com>
2022-07-23 10:45:03 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,7 +13,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LOdyoAgnFUG4JtXY9Yr5KBQ7aGQF96TAy9UzZkmgymNlwipTWjNoAsyliYrVlWZbT7ymYnQdufmbe4D0HLxMl2i00NZFQWrSf',
+        'client_secret': 'test_client_secret',
     }
 
     return render(request, template, context)
